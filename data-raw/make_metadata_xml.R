9fd2702e847e12e85007dd7194f1ff585b822c9b
alvizek <avizek@flowwest.com>
2024-06-21 08:05:23 -0700

    updates version name

--- data-raw/make_metadata_xml.R
+++ data-raw/make_metadata_xml.R
@@ -79,7 +79,7 @@ View(report_df)
 EMLaide::update_edi_package(user_id = Sys.getenv("EDI_USER_ID"),
                             password = Sys.getenv("EDI_PASSWORD"),
                             eml_file_path = paste0(edi_number, ".xml"),
-                            existing_package_identifier = "edi.1504.1.xml",
+                            existing_package_identifier = "edi.1504.2.xml",
                             environment = "production")
 
 # evaluate <- EMLaide::evaluate_edi_package(user_id = Sys.getenv("EDI_USER_ID"),
