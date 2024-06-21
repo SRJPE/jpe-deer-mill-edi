0cf45647f2b2e88f66562359fcd1ff962cb68f8e
alvizek <avizek@flowwest.com>
2024-06-21 07:47:13 -0700

    updates metadata xml

--- data-raw/make_metadata_xml.R
+++ data-raw/make_metadata_xml.R
@@ -72,8 +72,15 @@ EML::eml_validate(paste0(edi_number, ".xml"))
 
 EMLaide::evaluate_edi_package(Sys.getenv("EDI_USER_ID"), Sys.getenv("EDI_PASSWORD"), paste0(edi_number, ".xml"))
 View(report_df)
-EMLaide::upload_edi_package(Sys.getenv("EDI_USER_ID"), Sys.getenv("EDI_PASSWORD"), paste0(edi_number, ".xml"))
-
+EMLaide::upload_edi_package(Sys.getenv("EDI_USER_ID"),
+                            Sys.getenv("EDI_PASSWORD"),
+                            paste0("edi.1504.1.xml"),
+                            environment = "staging")
+EMLaide::update_edi_package(user_id = Sys.getenv("EDI_USER_ID"),
+                            password = Sys.getenv("EDI_PASSWORD"),
+                            eml_file_path = paste0(edi_number, ".xml"),
+                            existing_package_identifier = paste0("edi.1504.1.xml"),
+                            environment = "production")
 
 # evaluate <- EMLaide::evaluate_edi_package(user_id = Sys.getenv("EDI_USER_ID"),
 #                                           password = Sys.getenv("EDI_PASSWORD"),
